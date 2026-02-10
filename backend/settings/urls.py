b1288a393b1bd63009ddb79a0add4071c97ccfd9
aconcore <akniet.talgat.05@mail.ru>
2026-02-10 23:19:12 +0500

    fixed: Settings

--- backend/settings/urls.py
+++ backend/settings/urls.py
@@ -18,15 +18,15 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-from drf_spectacular.views import SpectacularAPIView,SpectacularSwaggerView,SpectacularRedocView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/',include('apps.auths.urls')),
     path('api/v2/',include('apps.main.urls')),
     
-    path('api/schema/',SpectacularAPIView.as_view(),name='schema'),
-    path('api/schema/swagger/',SpectacularSwaggerView.as_view(),name='schema-swagger'),
-    path('api/schema/redoc/',SpectacularRedocView.as_view(),name='schema-redoc'),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='schema-swagger'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='schema-redoc'),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
